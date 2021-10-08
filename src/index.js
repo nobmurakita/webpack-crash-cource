f5064b89f38cdf8f14da2af633859f3f32a23970
nobmurakita <nobmurakita@users.noreply.github.com>
2021-10-09 00:42:50 +0900

    apply uglifyjs-webpack-plugin

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDom from 'react-dom';
 import './style.css';
 import './style.scss';
 
+console.log('I am printng logs.');
+
 ReactDom.render(
   <div>Hello, React!</div>,
   document.getElementById('root')
